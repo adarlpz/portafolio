03c561d71868f743998d179db182975de096cd9c
adarlpz <adarlopz.dev@gmail.com>
2025-02-10 12:58:43 -0600

    globales

---
+++ global/portafolio_global.js
@@ -0,0 +1,39 @@
+document.addEventListener("DOMContentLoaded", function () {
+    cargarContenido("/global/portafolio_global.html", "header-container", "header");
+    cargarContenido("/global/portafolio_global.html", "footer-container", "footer");
+    cargarLinksGlobales("/global/portafolio_global.html");
+
+    setTimeout(() => {
+        document.getElementById("year").textContent = new Date().getFullYear();
+    }, 25);
+});
+function cargarContenido(url, contenedorId, selector) {
+    fetch(url)
+        .then(response => response.text())
+        .then(data => {
+            let tempDiv = document.createElement("div");
+            tempDiv.innerHTML = data;
+
+            let elemento = tempDiv.querySelector(selector);
+            if (elemento) {
+                document.getElementById(contenedorId).innerHTML = elemento.outerHTML;
+            }
+        })
+        .catch(error => console.error("Error cargando " + selector, error));
+}
+function cargarLinksGlobales(url) {
+    fetch(url)
+        .then(response => response.text())
+        .then(data => {
+            let tempDiv = document.createElement("div");
+            tempDiv.innerHTML = data;
+            let links = tempDiv.querySelectorAll('link[rel="stylesheet"], link[rel="preconnect"], link[rel="shortcut icon"]');
+
+            links.forEach(link => {
+                if (!document.head.querySelector(`link[href="${link.href}"]`)) {
+                    document.head.appendChild(link.cloneNode(true)); // Agregar solo si no existe
+                }
+            });
+        })
+        .catch(error => console.error("Error cargando links globales", error));
+}
